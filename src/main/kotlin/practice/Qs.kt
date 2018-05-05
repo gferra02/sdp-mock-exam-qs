fd6654b77082735a76f28da41797673d6994fd23
Matthew Ralph <ralpmat@gmail.com>
2018-05-05 16:44:06 +0100

    Update

--- src/main/kotlin/practice/Qs.kt
+++ src/main/kotlin/practice/Qs.kt
@@ -1,5 +1,6 @@
 package practice
 
+// q1
 fun manyTimesString(s: String, i: Int): String {
   tailrec fun loop(stringSoFar: String, i: Int): String {
     return if (i == 0) stringSoFar else loop(stringSoFar + s, i - 1)
@@ -7,5 +8,16 @@ fun manyTimesString(s: String, i: Int): String {
   return loop (s, i - 1)
 }
 
+// q2
 data class SimpleTime(val hours: Int, val minutes: Int)
 val t = SimpleTime(hours=5, minutes=30)
+
+// q3
+data class Item(val name: String, val price: Double) {
+  fun cost(
+    grocery: Boolean = false,
+    medication: Boolean = false,
+    taxRate: Double = 0.10): Double {
+      return if (grocery || medication) price else price * (1 + taxRate)
+    }
+}
--- src/test/kotlin/test/PracticeTests.kt
+++ src/test/kotlin/test/PracticeTests.kt
@@ -2,10 +2,14 @@ package test
 import practice.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 
 class PracticeTests {
-  @Test
-  fun q1() {
+  // For comparing Doubles - we don't care if they are slightly out
+  // due to floating point arithmetic rounding.
+  private infix fun Double.nearEnough(d2: Double) = this - d2 < 0.001
+
+  @Test fun q1() {
     val m1 = manyTimesString("abc", 3)
     assertEquals("abcabcabc", m1)
   }
@@ -14,5 +18,14 @@ class PracticeTests {
     val t = SimpleTime(hours=5, minutes=30)
     assertEquals(5, t.hours)
     assertEquals(30, t.minutes)
+  }
+
+  @Test fun q3() {
+    val flour = Item(name="flour", price=4.0)
+    assertEquals(4.0, flour.cost(grocery=true))
+    val sunscreen = Item(name="sunscreen", price=3.0)
+    assertTrue(sunscreen.cost() nearEnough 3.3)
+    val tv = Item(name="television", price=500.0)
+    assertTrue(tv.cost(taxRate = 0.06) nearEnough 530.0)
   }
 }
