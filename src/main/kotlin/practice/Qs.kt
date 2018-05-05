8f626f4e7550daeeda03c4dba0556eb63d60af14
Matthew Ralph <ralpmat@gmail.com>
2018-05-05 16:09:48 +0100

    Initial commit of exam test qs

---
+++ src/main/kotlin/practice/Qs.kt
@@ -0,0 +1,8 @@
+package practice
+
+fun manyTimesString(s: String, i: Int): String {
+  tailrec fun loop(stringSoFar: String, i: Int): String {
+    return if (i == 0) stringSoFar else loop(stringSoFar + s, i - 1)
+  }
+  return loop (s, i - 1)
+}
---
+++ src/test/kotlin/test/PracticeTests.kt
@@ -0,0 +1,13 @@
+package test
+import practice.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+
+class PracticeTests {
+  @Test
+  fun t1() {
+    val m1 = manyTimesString("abc", 3)
+    assertEquals("abcabcabc", m1)
+  }
+
+}
